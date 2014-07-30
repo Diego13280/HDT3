8666f175d23bb8116bd21f4a804d20d0e55b1c92
esterafalatin <stevraffs@gmail.com>
2014-07-29 23:07:26 -0600

    Si

--- HDT3/src/Archivo.java
+++ HDT3/src/Archivo.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import java.io.File; 
 import java.io.PrintWriter; 
 import java.util.Random; 
@@ -45,48 +45,3 @@ public Archivo()
     { 
         JOptionPane.showMessageDialog(null, e.getMessage()); 
     } 
-
-=======
-import java.io.File; 
-import java.io.PrintWriter; 
-import java.util.Random; 
-import javax.swing.JOptionPane;
-
-/** 
-* 
-* @author Diego Juarez		13280
-* @author Daniel Mejia		13271
-* @author Esteban Barrera	13413
-*/ 
-public class Archivo { 
-private File archivo; 
-
-public Archivo() 
-{ 
-archivo = new File("miarchivo.txt");
-try 
-{ 
-PrintWriter grabador = new PrintWriter(archivo);//aqui creamos 
-//el objeto que permite grabar en el archivo 
-
-Random rand = new Random();
-
-int contador = 0;
-while (contador < 2000){
-
-    int x = rand.nextInt(1000);
-    grabador.println(x); 
-    contador ++;
-}
-
-
-grabador.close();//es muy importante cerrar el archivo ya que 
-//sino lo haces no se guardan los cambios 
-} 
-catch(Exception e) 
-{ 
-JOptionPane.showMessageDialog(null, e.getMessage()); 
-} 
-
->>>>>>> origin/master
-} }
