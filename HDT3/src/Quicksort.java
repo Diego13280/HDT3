cd3ed6b8cfe30515871d74ab293c9baf8e5412a7
esterafalatin <stevraffs@gmail.com>
2014-07-31 14:28:40 -0600

    QuickSort

---
+++ HDT3/src/Quicksort.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Esteban
+ */
+public class Quicksort 
+{
+    public static void swap(int data[], int i, int j)
+// pre: 0 <= i,j < data.length
+// post: data[i] and data[j] are exchanged
+    {
+        int temp;
+        temp = data[i];
+        data[i] = data[j];
+        data[j] = temp;
+    }
+    private static int partition(int data[], int left, int right)
+    {
+       // pre: left <= right 
+       // post: data[left] placed in the correct (returned) location
+       
+       while(true)
+       {
+           // move right "pointer" toward left
+           while (left < right && data[left] < data[right]) right--;
+           if (left < right) swap(data,left++,right);
+           else return left;
+           // move left pointer toward right
+           while (left < right && data[left] < data[right]) left++;
+           if (left < right) swap(data,left,right--);
+           else return right;
+           
+       }
+    }
+    
+    public static void quickSort(int data[], int n)
+    // post: the values in data[0..n-1] are in ascending order
+    {
+        quickSortRecursive(data,0,n-1);
+    }
+    private static void quickSortRecursive(int data[],int left,int right)
+    // pre: left <= right
+    // post: data[left..right] in ascending order
+    {
+        int pivot; // the final location of the leftmost value
+        if (left >= right) return;
+        pivot = partition(data,left,right); /* 1 - place pivot */
+        quickSortRecursive(data,left,pivot-1); /* 2 - sort small */
+        quickSortRecursive(data,pivot+1,right);/* 3 - sort large */
+        /* done! */
+    }   
+    
+}
--- HDT3/src/principal.java
+++ HDT3/src/principal.java
@@ -1,21 +1,37 @@
+
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+ /*El código para la lectura del archivo de texto fue tomado de la página: http://codigosparadesarrolladores.blogspot.com/2014/06/codigo-java-leer-archivos-de-texto.html
+ * y tambien  http://codigomaldito.blogspot.com/2011/06/como-leer-un-archivo-de-texto-en-java.html
 
 /**
  *
  * @author Administrador
  */
-public class principal {
 
+public class principal {
+    private static int numeroRecibido;
+    private static String strLinea;
+    private static String strLinea1;
+    private Numeros[] numeros;
+    private int cont;
+    
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
         Archivo archivo =new Archivo(); 
+        /**
+         * Se lee la cadena ingresada en un archivo de texto
+         */
     }
-    
 }
